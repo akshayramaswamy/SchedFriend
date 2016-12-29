02ed4db0b9aabe391cfcaf2c5d1c048e414c31d7
chetankr <chet2an@gmail.com>
2016-12-29 12:30:06 -0800

    fixed publication/subscription bug for users

--- server/user-data-pubs.js
+++ server/user-data-pubs.js
@@ -1,22 +1,26 @@
-Meteor.publish('userData', function() {
-     var currentUser;
-     currentUser = this.userId;
-     if (currentUser) {
-         return Meteor.users.find({
-             _id: currentUser
-         }, {
-         fields: {
-             // Default
-             "emails": 1,
-             // Created profile property
-             "profile": 1,
-//             "currentCourses":1,
-//	     "university":1,
-//	     "profPicture":1,
-	     
-         }
-      });
-    } else {
-      return this.ready();
-  }
+Meteor.publish('userList', function (){ 
+  return Meteor.users.find({});
 });
+
+// Meteor.publish('userData', function() {
+//      var currentUser;
+//      currentUser = this.userId;
+//      if (currentUser) {
+//         return Meteor.users.find({
+//              _id: currentUser
+//          }, {
+//          fields: {
+//              // Default
+//              "emails": 1,
+//              // Created profile property
+//              "profile": 1,
+// //             "currentCourses":1,
+// //	     "university":1,
+// //	     "profPicture":1,
+	     
+//          }
+//       });
+//     } else {
+//       return this.ready();
+//   }
+// });
